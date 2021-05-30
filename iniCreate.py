7265bb71770a7556d4034568e9dfea809429c685
Alexey <Bedrin18@mail.ru>
2021-05-30 17:54:59 +0300

    ini file creation

---
+++ iniCreate.py
@@ -0,0 +1,9 @@
+import configparser
+
+config = configparser.ConfigParser()
+config['VK_MSG'] = {'Token': 'filler', 'Session': 'filler', 'Key': 'filler', 'Server': 'filler', 'ts': 'filler'}
+config['DB'] = {'Host': 'filler', 'Database': 'filler', 'User': 'filler', 'Password': 'filler'}
+
+with open('params.ini', 'w') as configfile:
+    config.write(configfile)
+
